6a1419886c18dc82fa2b1ed5df88eb5a6b9c29ad
Tara Vancil <tbvanc@gmail.com>
2016-10-27 11:10:14 -0400

    set up oauth

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ import {OAuth} from 'oauth'
 
 const app = express()
 
+const API_KEY = process.env.TWITTER_API_KEY
+const API_SECRET = process.env.TWITTER_API_SECRET
+const ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN
+const ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+const oauth = new OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  API_KEY,
+  API_SECRET,
+  '1.0A,
+  null,
+  'HMAC-SHA1'
+)
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
