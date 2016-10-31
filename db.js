e3a106e40e73d023de89bb9014e41d9a806a92e0
Tara Vancil <tbvanc@gmail.com>
2016-10-31 15:56:39 -0400

    add function to get screen name by id

--- db.js
+++ db.js
@@ -66,6 +66,15 @@ export function getTweetByTweetID (tweetID) {
   })
 }
 
+export function getScreenNameByID (id) {
+  return new Promise ((resolve, reject) => {
+    db.get(`SELECT screen_name FROM users WHERE id = ?`, id, (err, row) => {
+      if (err) reject(err)
+      row === undefined ? resolve(null) : resolve(row.screen_name)
+    })
+  })
+}
+
 function getUserIDByScreenName (screenName) {
   return new Promise ((resolve, reject) => {
     db.get(`SELECT * FROM users WHERE screen_name = ?`, screenName, (err, row) => {
