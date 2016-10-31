cdd08bf83ed057e041b7cd51970c3b00fb9bbd01
Tara Vancil <tbvanc@gmail.com>
2016-10-30 20:52:12 -0400

    render feedback while tweets are being fetched

--- listeners.js
+++ listeners.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const tweetsList = document.getElementById('tweets')
   const fetchMoreTweets = document.getElementById('fetch-more-tweets')
+  const fetchingFeedback = document.createElement('p')
+  fetchingFeedback.innerText = 'Fetching more tweets...'
 
   fetchMoreTweets.onclick = (e) => {
     e.preventDefault()
 
+    tweetsList.appendChild(fetchingFeedback)
     const req = new XMLHttpRequest()
 
     req.onload = () => {
+      tweetsList.removeChild(fetchingFeedback)
       tweetsList.innerHTML += req.response
     }
 
