066052d726cf070da309a16b9596f3089db589d1
Tara Vancil <tbvanc@gmail.com>
2016-11-04 13:20:02 -0400

    improve readability

--- components/translation.js
+++ components/translation.js
@@ -1,25 +1,37 @@
 export function renderTranslation (t, voteCount, user) {
-  const authorLink = `<a href='/user/${t.author_screen_name}'>${t.author_screen_name}</a>`
+  const authorLink = `<a href='/user/${t.author_screen_name}'>` +
+                     `${t.author_screen_name}</a>`
+
   const date = new Date(t.timestamp).toLocaleString()
 
   let deleteForm = ''
 
   if (user && (t.uid === user.id || user.screenName === 'taravancil')) {
     deleteForm = `<form action='/remove-translation' method='POST'><input` +
-                 ` type='hidden' name='id' value='${t.id}' /><button` +
+                 ` type='hidden' name='id' value='${t.id}' /><input` +
+                 ` type='hidden' name='parent' value='${t.parent}' /><button` +
                  ` type='submit' class='btn btn--link'>Delete</button></form>`
   }
 
   let votingDisabled = ''
-  if (!user) votingDisabled = '' //disabled'
+  if (!user) votingDisabled = 'disabled'
 
-  const upvoteBtn = `<button id='upvote-${t.id}' class='vote-btn' data-delta='1' data-id='${t.id}' ${votingDisabled}><i class='arrow arrow--up' aria-hidden='true'></i></button>`
-  const downvoteBtn = `<button id='downvote-${t.id}' class='vote-btn' data-delta='-1' data-id='${t.id}' ${votingDisabled}><i class='arrow arrow--down' aria-hidden='true'></i></button>`
+  const upvoteBtn = `<button id='upvote-${t.id}' class='vote-btn' ` +
+                    `data-delta='1' data-id='${t.id}' ${votingDisabled}>` +
+                    `<i class='arrow arrow--up' aria-hidden='true'></i>` +
+                    `</button>`
 
-  const votingForm = `<div class='vote'>${upvoteBtn}<span id='votes-count-${t.id}' class='votes count'>${voteCount}</span>${downvoteBtn}</div>`
+  const downvoteBtn = `<button id='downvote-${t.id}' class='vote-btn'` +
+                      `data-delta='-1' data-id='${t.id}' ${votingDisabled}>` +
+                      `<i class='arrow arrow--down' aria-hidden='true'></i>` +
+                      `</button>`
 
-  return `<li class='translation'><div class='translation__meta'>` +
-         `${authorLink} ${date}${deleteForm}<div class='votes'>` +
-         `${votingForm}</div><div ` +
-         `class='translation__text'>${t.translation}</div></li>`
+  const votingForm = `<div class='vote'>${upvoteBtn}<span ` +
+                     `id='votes-count-${t.id}' class='votes count'>` +
+                     `${voteCount}</span>${downvoteBtn}</div>`
+
+  return `<div class='translation'><div class='translation__meta'>` +
+         `${authorLink} ${date}${deleteForm}</div>${t.comment}<div ` +
+         `class='translation__text'>${t.translation}<div class='votes'>` +
+         `${votingForm}</div></div></div>`
 }
