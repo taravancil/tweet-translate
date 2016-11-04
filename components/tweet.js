3c51e5fa8a23246d3c5a5bd9ea43b8d453c00ec5
Tara Vancil <tbvanc@gmail.com>
2016-11-04 13:21:55 -0400

    add option to render link to translations

--- components/tweet.js
+++ components/tweet.js
@@ -1,19 +1,18 @@
-function renderTweetActions (tweet, user, translationCount) {
+function renderTweetActions (tweet, user, translationCount, showLink) {
   let addTranslationBtn = ''
   if (user) {
     addTranslationBtn = `<button name="add-translation" class="btn btn--link"` +
                         `data-id='${tweet.id}' data-parent='${tweet.tweet_id}'` +
                         `data-type='translation'>Add Translation</button>`
-
   }
 
-  const link = `<a href=/tweet/${tweet.tweet_id} class='btn btn--link'>Translations (${translationCount})</a>`
+  const link = showLink ? `<a href=/tweet/${tweet.tweet_id} class='btn btn--link'>Translations (${translationCount})</a>` : ''
 
   return `<div id='tweet-links-${tweet.id}' class='tweet-links'>` +
          `${addTranslationBtn}${link}</div>`
 }
 
-export function renderTweet (tweet, user, translationCount) {
-  const links = renderTweetActions(tweet, user, translationCount)
+export function renderTweet (tweet, user, translationCount, showLink) {
+  const links = renderTweetActions(tweet, user, translationCount, showLink)
   return `<div id='tweet-${tweet.id}' class='tweet'>${tweet.html}${links}</div>`
 }
