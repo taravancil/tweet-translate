ff48a2f206e8b98e0bcf3aeca5c025723757a807
Tara Vancil <tbvanc@gmail.com>
2016-11-07 15:02:12 -0500

    add user/screename endpoint

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ import {
   getTweetByTweetID,
   getScreenNameByID,
   getTranslationAuthorID,
+  getTranslationsByUserID,
   getTranslationCount,
+  getUserIDByScreenName,
   addVote,
   getVoteCount
 } from './db'
@@ -156,6 +158,33 @@ app.get('/tweet/:tweetID', async (req, res) => {
     })
   } catch (err) {
     console.error(err)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+app.get('/user/:screenName', async (req, res) => {
+  // check that the user exists
+  const screenName = req.params.screenName
+  const uid = await getUserIDByScreenName(screenName)
+  if (uid === undefined) res.status(404).send('Page Not Found')
+
+  let content = `<h2>${screenName}'s Translations</h2>`
+
+  try {
+    const translations = await getTranslationsByUserID(uid)
+    if (!translations.length) {
+      content += '<p>No translations</p>'
+    } else {
+      let translationEls = ''
+      for (const t of translations) {
+        const voteCount = await getVoteCount(t.id)
+        translationEls += renderTranslation(t, voteCount, req.session.user)
+      }
+      content += `<ul class='translations'>${translationEls}</ul>`
+    }
+
+    res.render('layout', {content: content, user: req.session.user})
+  } catch (err) {
     res.status(500).send('Internal Server Error')
   }
 })
