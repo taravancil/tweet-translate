7456b3e5133d935381aa38062ca9dfc5d6105f1e
Tara Vancil <tbvanc@gmail.com>
2016-11-07 13:38:33 -0500

    set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp')
+const pump = require('pump')
+const babel = require('gulp-babel')
+const browserify = require('gulp-browserify')
+const cleanCSS = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
+
+const clientScripts = ['dist/home.js', 'dist/translations.js', 'dist/tweet.js']
+
+gulp.task('transpile-js', (cb) => {
+  return pump([
+    gulp.src(['index.js', 'lib/*.js', 'lib/**/*.js']),
+    babel(),
+    gulp.dest('dist')
+  ])
+})
+
+gulp.task('bundle-js', ['transpile-js'], (cb) => {
+  pump([
+    gulp.src(clientScripts),
+    browserify(),
+    gulp.dest('dist')
+  ])
+})
+
+gulp.task('build-css', () => {
+  pump([
+    gulp.src('css/*.css'),
+    cleanCSS(),
+    gulp.dest('dist')
+  ])
+})
+
+gulp.task('build', ['transpile-js', 'bundle-js', 'build-css'])
